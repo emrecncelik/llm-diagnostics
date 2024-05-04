35e648faffe17de1c8d478a2fb6558d2f77cfefd
emrecncelik <emrecncelik@gmail.com>
2024-05-05 00:15:52 +0300

    dump prompted preds for now,

--- llm-diagnostics/datasets.py
+++ llm-diagnostics/datasets.py
@@ -1,36 +1,69 @@
+import os
+import sys
+import logging
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
+from transformers import AutoTokenizer
+
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+    stream=sys.stdout,
+)
+logger = logging.getLogger(os.path.basename(__file__))
 
 
 class NegationDataset(Dataset):
     def __init__(
         self,
-        filename,
-        prompt_template="""
-            The goal is to complete the given sentence with one English word.
-            DO NOT generate punctuation or new line characters or underlines.
-            Sentence: {context}
-            Answer:
-            """,
+        filename: str,
+        tokenizer: AutoTokenizer,
+        max_length: int = 30,
+        prompt_template=None,
     ):
+        # Tokenizer setup
+        if tokenizer.pad_token is None:
+            tokenizer.pad_token = tokenizer.eos_token
+            logger.warning("Setting tokenizer's pad_token to eos_token")
 
+        # Load dataset
         if filename.endswith("tsv"):
             self.dataset = pd.read_csv(filename, sep="\t")
         else:
             self.dataset = pd.read_csv(filename)
-        self.contexts = (
+        contexts = (
             self.dataset["context_aff"].tolist() + self.dataset["context_neg"].tolist()
         )
-        self.targets = (
+        targets = (
             self.dataset["target_aff"].tolist() + self.dataset["target_neg"].tolist()
         )
         self.prompt_template = prompt_template
+
+        if prompt_template is None:
+            contexts_tokenized = tokenizer(
+                contexts,
+                max_length=max_length,
+                padding="max_length",
+            )
+            targets_tokenized = tokenizer(
+                targets,
+                padding=False,  # assuming single token target, might change later
+            )
+            self.input_ids = contexts_tokenized["input_ids"]
+            self.attention_mask = contexts_tokenized["attention_mask"]
+            self.target_ids = targets_tokenized["target_ids"]
+        else:
+            raise NotImplementedError("Prompt-based tokenization not implemented")
 
     def __len__(self):
         return len(self.contexts)
 
     def __getitem__(self, idx):
         return (
-            self.prompt_template.format(context=self.contexts[idx]),
-            self.targets[idx],
+            {
+                "input_ids": self.input_ids[idx],
+                "attention_mask": self.attention_mask[idx],
+            },
+            self.target_ids[idx],
         )
