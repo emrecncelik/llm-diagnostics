e56ddede6e97efb9c075fec89f992b80524e2718
emrecncelik <emrecncelik@gmail.com>
2024-05-21 12:54:39 +0300

    hacky eval script, fix it if u have the will to live

--- eval.py
+++ eval.py
@@ -1,8 +1,10 @@
+import os
 import torch
 import warnings
 import argparse
-from llm_diagnostics.evaluate import LLMDiagnosticsEvaluator, format_results
+from llm_diagnostics.evaluation import LLMDiagnosticsEvaluator, Metric
 from llm_diagnostics.config import DATASETS
+from llm_diagnostics.utils import format_results
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -17,23 +19,8 @@ def get_args():
     )
     parser.add_argument("--quantization", action="store_true", help="Quantize model")
     parser.add_argument(
-        "--data_dir", default="datasets", help="Directory containing the dataset files"
-    )
-    parser.add_argument(
-        "--dataset",
-        required=True,
-        choices=list(DATASETS.keys()),
-        help="Identifier of dataset to evaluate",
-    )
-    parser.add_argument(
-        "--simplify_a_an",
-        default=None,
-        type=str,
-        help="Simplify (a|an) in context to this value (simplified depending on target if set to 'adaptive')",
-    )
-    parser.add_argument(
         "--batch_size",
-        default=16,
+        default=4,
         type=int,
         help="Batch size for evaluation",
     )
@@ -55,37 +42,104 @@ def get_args():
 
 if __name__ == "__main__":
     args = get_args()
-    evaluator = LLMDiagnosticsEvaluator(
-        "llama2_test", args.data_dir, output_dir="outputs"
-    )
+
+    evaluator = LLMDiagnosticsEvaluator(data_dir="datasets")
     evaluator.load_model(
-        args.model_name,
-        quantization=args.quantization,
+        model_name=args.model_name,
+        quantization=args.quantization if "bert" not in args.model_name else False,
         token=args.hf_token,
         cache_dir=args.hf_cache_dir,
     )
-    evaluator.load_dataset(
-        args.dataset,
-        simplify_a_an=args.simplify_a_an,
-        masked=True if evaluator.task_type == "maskedlm" else False,
-        target_prefix=" " if "mamba" in args.model_name else "",
-    )
-    targets, preds, logits = evaluator.run_inference(
-        topk=[1, 3, 5, 10, 20],
-        batch_size=args.batch_size,
-        use_generate=False,  # use_generate
-        progress_bar=args.progress_bar,
-        device="cuda" if torch.cuda.is_available() else "cpu",
-        negative_or_reversed=False,
-    )
-    topk_accuracies = evaluator.compute_accuracy(
-        targets=targets,
-        preds=preds,
-        topk=[1, 3, 5, 10, 20],
-    )
 
-    results = evaluator.format_results(targets, preds, False)
-    print(f"Accuracy: {topk_accuracies}")
-    print(results.head())
-    print(logits)
-    results.to_csv(f"results_{args.dataset}.csv", index=False)
+    for dataset in DATASETS.keys():
+        datasets = evaluator.load_dataset(
+            dataset=dataset,
+            simplify_a_an=DATASETS[dataset]["simplify_a_an"],
+            masked=True if evaluator.task_type == "maskedlm" else False,
+            target_prefix=" " if "mamba" in args.model_name else "",
+        )
+
+        targets, preds, probs = evaluator.run_inference(
+            topk=[1, 3, 5, 10, 20],
+            batch_size=args.batch_size,
+            progress_bar=args.progress_bar,
+            device="cuda" if torch.cuda.is_available() else "cpu",
+            negative_or_reversed=False,
+        )
+        targets_hat, preds_hat, probs_hat = evaluator.run_inference(
+            topk=[1, 3, 5, 10, 20],
+            batch_size=args.batch_size,
+            progress_bar=args.progress_bar,
+            device="cuda" if torch.cuda.is_available() else "cpu",
+            negative_or_reversed=True,
+        )
+
+        accuracy = Metric.topk_accuracy(targets, preds, [1, 3, 5, 10, 20])
+        accuracy_hat = Metric.topk_accuracy(targets_hat, preds_hat, [1, 3, 5, 10, 20])
+        sensitivity = Metric.sensitivity_ettinger(
+            targets, targets_hat, probs, probs_hat, False, 0, True
+        )
+        sensitivity_hat = Metric.sensitivity_ettinger(
+            targets, targets_hat, probs, probs_hat, False, 0, True
+        )
+
+        sensitivity_th = Metric.sensitivity_ettinger(
+            targets, targets_hat, probs, probs_hat, False, 0.01, True
+        )
+        sensitivity_hat_th = Metric.sensitivity_ettinger(
+            targets, targets_hat, probs, probs_hat, False, 0.01, True
+        )
+        if "neg" in dataset:
+            negation_sensitivity = Metric.sensitivity_negation_shivagunde(
+                preds, preds_hat
+            )
+
+        result_string = ""
+        result_string += f"Model: {args.model_name}\n"
+        result_string += f"Dataset: {dataset}\n"
+        result_string += f"Accuracy: {accuracy}\n"
+        result_string += f"Accuracy (negative_or_reversed): {accuracy_hat}\n"
+        result_string += f"Sensitivity: {sensitivity}\n"
+        result_string += f"Sensitivity (negative_or_reversed): {sensitivity_hat}\n"
+        result_string += f"Sensitivity (threshold=0.01): {sensitivity_th}\n"
+        result_string += f"Sensitivity (negative_or_reversed, threshold=0.01): {sensitivity_hat_th}\n"
+        if "neg" in dataset:
+            result_string += f"Negation Sensitivity (Shivagunde et al., 2023): {negation_sensitivity}\n"
+
+        if not os.path.exists(f"{args.model_name.replace('/', '_')}"):
+            os.mkdir(f"{args.model_name.replace('/', '_')}")
+
+        with open(
+            f"{args.model_name.replace('/', '_')}/results_{dataset}.txt", "w"
+        ) as f:
+            f.write(result_string)
+
+        print(result_string)
+
+        prediction_results = format_results(
+            dataset=evaluator.datasets[0],
+            tokenizer=evaluator.tokenizer,
+            target_ids=targets,
+            pred_ids=preds,
+        )
+
+        prediction_results_hat = format_results(
+            dataset=evaluator.datasets[1],
+            tokenizer=evaluator.tokenizer,
+            target_ids=targets_hat,
+            pred_ids=preds_hat,
+        )
+
+        print(prediction_results.head())
+        print(prediction_results_hat.head())
+
+        prediction_results.to_csv(
+            f"{args.model_name.replace('/', '_')}/predictions_{dataset}.csv",
+            index=False,
+        )
+        prediction_results_hat.to_csv(
+            f"{args.model_name.replace('/', '_')}/predictions_{dataset}_negative_or_reversed.csv",
+            index=False,
+        )
+
+        evaluator.datasets = []
